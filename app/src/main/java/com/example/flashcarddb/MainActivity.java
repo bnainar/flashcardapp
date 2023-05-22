81e3a462c0a6d815ebb6022727a9a5d4d2323a28
Mohamed Nainar <nainar.mb@gmail.com>
2023-05-22 17:07:56 +0530

    add imgs

--- app/src/main/java/com/example/flashcarddb/MainActivity.java
+++ app/src/main/java/com/example/flashcarddb/MainActivity.java
@@ -67,6 +67,13 @@ public class MainActivity extends AppCompatActivity {
 
         db = openOrCreateDatabase("FlashCardDB", Context.MODE_PRIVATE, null);
         db.execSQL("CREATE TABLE IF NOT EXISTS cards(id INTEGER PRIMARY KEY AUTOINCREMENT, title VARCHAR NOT NULL, body VARCHAR NOT NULL);");
+//        db.execSQL("DELETE FROM cards");
+//        db.execSQL("INSERT INTO cards(title, body) VALUES ('What is the definition of \"ephemeral\"?', '\"Ephemeral\" refers to something that lasts for a short period of time.');");
+//        db.execSQL("INSERT INTO cards(title, body) VALUES ('What year did World War II end?', 'World War II ended in 1945.');");
+//        db.execSQL("INSERT INTO cards(title, body) VALUES ('Name the largest desert in the world.', 'The largest desert in the world is the Sahara Desert.');");
+//        db.execSQL("INSERT INTO cards(title, body) VALUES ('Who is the protagonist in the novel \"To Kill a Mockingbird\"?', ' The protagonist is Jean Louise \"Scout\" Finch.');");
+//        db.execSQL("INSERT INTO cards(title, body) VALUES ('Name the process by which plants convert sunlight into energy.', 'The process is called photosynthesis.');");
+//        db.execSQL("INSERT INTO cards(title, body) VALUES ('Define the word \"exacerbate.\"', '\"Exacerbate\" means to make a situation or problem worse.');");
         showAllCards();
     }
 
